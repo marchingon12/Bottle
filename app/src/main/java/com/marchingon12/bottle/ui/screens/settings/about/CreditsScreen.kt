0960c5c6b8ca96a655c416d36dc8bb6bafb5b9b8
marchingon12 <augustthegreat.cool@gmail.com>
2023-02-09 02:10:50 +0100

    added more screens
    Signed-off-by: marchingon12 <augustthegreat.cool@gmail.com>

--- app/src/main/java/com/marchingon12/bottle/ui/screens/settings/about/CreditsScreen.kt
+++ app/src/main/java/com/marchingon12/bottle/ui/screens/settings/about/CreditsScreen.kt
@@ -1,1 +1,2 @@
-package com.marchingon12.bottle.ui.screens.settings.abo
+package com.marchingon12.bottle.ui.screens.settings.about
+
--- app/src/main/java/com/marchingon12/bottle/ui/screens/settings/general/GeneralScreen.kt
+++ app/src/main/java/com/marchingon12/bottle/ui/screens/settings/general/GeneralScreen.kt
@@ -1,3 +1,2 @@
 package com.marchingon12.bottle.ui.screens.settings.general
 
-// TODO: add components for transparent social icon buttons on screen below
